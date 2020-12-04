c2e5aea49c6cd9e9c1542933568dc2cd9e21a4b2
hmarely76@gmail.com <hmarely76@gmail.com>
2020-12-04 13:12:19 -0600

    updating our app a shop page and menu item collections

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 import { Switch, Route } from 'react-router-dom';
 
-const HatsPage = ()=> (
 
-  <div><h1>Hats Page</h1></div>
-);
 
 function App() {
   return (
     <div>
+    <Header/>
     <Switch>
     <Route exact path ='/' component={HomePage}/>
-    <Route path ='/hats' component={HatsPage}/>
+    <Route path ='/shop' component={ShopPage}/>
     </Switch>
     
     </div>
