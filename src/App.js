1a5b87669be2c71ea32af7337091ce3deeddfab9
hmarely76@gmail.com <hmarely76@gmail.com>
2020-12-10 08:55:25 -0600

    finish sign in handle with email and password

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
 import SignInSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import Header from './components/header/header.component.jsx';
-import { auth } from './firebase/firebase.utils.jsx';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils.jsx';
 
 
 
@@ -23,9 +23,20 @@ class App extends React.Component {
 
   
   componentDidMount() {
-    this.unsubscribeFromAuth=auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
-      console.log(user);
+    this.unsubscribeFromAuth=auth.onAuthStateChanged(async userAuth => {
+      if (userAuth){
+        const userRef= await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+        this.setState({
+          currentUser:{
+            id:snapShot.id,
+            ...snapShot.data()
+          }
+      
+        });
+      });
+      }
+      this.setState({currentUser: userAuth});
     });
   }
   componentWillUnmount(){
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component.jsx';
 import CustomButton from '../custom-button/custom-button.component.jsx';
-import { signInWithGoogle } from '../../firebase/firebase.utils.jsx';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils.jsx';
 
 
 class SignIn extends React.Component {
@@ -16,9 +16,17 @@ class SignIn extends React.Component {
        
     }
    
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({email:'', password:''});
+        const {email, password} = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email:'', password: ''});
+        } catch(error) {
+            console.log(error);
+        }
+        
+
     };
 
     handleChange = event => {
@@ -58,7 +66,7 @@ class SignIn extends React.Component {
           <div className='buttons'>
             <CustomButton 
             type='submit'> Sign In</CustomButton>
-            <CustomButton 
+            <CustomButton type="button"
             onClick={signInWithGoogle} isGoogleSignIn> {' '} Sign In with Google {' '}</CustomButton></div>
             </form>
             </div>
