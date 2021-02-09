bb0528f78ba097696440fbb74d874b07babb2c21
hmarely76@gmail.com <hmarely76@gmail.com>
2021-02-09 13:07:02 -0600

    updated shopdata with new data and add cart utils to cart reduce for grouping cartitems when you shop

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,9 @@
 import CartActionTypes from './cart.types';
+import {addItemToCart} from './cart.utils';
 
 const INITIAL_STATE = {
-    hidden:true
+    hidden:true,
+    cartItems:[]
 };
 
 const cartReducer = (state = INITIAL_STATE, action)=> {
@@ -9,10 +11,14 @@ const cartReducer = (state = INITIAL_STATE, action)=> {
     switch (action.type){
         case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
-                ... state,
+                ...state,
                 hidden: !state.hidden
             }
-
+            case CartActionTypes.ADD_ITEM:
+                return {
+                    ...state,
+                    cartItems: addItemToCart(state.cartItems, action.payload)
+                }
         default : 
         return state;
 
